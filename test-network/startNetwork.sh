59470107f029d5afa96ac967cf711852ec77a397
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-08-09 13:08:47 +0700

    build: create startNetwork.sh bash script

---
+++ test-network/startNetwork.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./network.sh down
+
+./network.sh up createChannel -s couchdb -c mychannel -ca
+
+./network.sh deployCC -ccn ledger -ccp ../mtcn-chaincode-go/ -ccl go -ccep "OR('Org1MSP.peer','Org2MSP.peer')"
+
+# ./network.sh deployCC -ccn ledger -ccp ../mtcn-chaincode-go/ -ccl go -ccep "AND('Org1MSP.peer','Org2MSP.peer')"
