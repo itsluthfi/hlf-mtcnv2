3f8474b4555ed80d7560553cca79043ff9ee62a9
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-08-30 17:48:42 +0700

    build: update startNetwork.sh bash script

--- test-network/startNetwork.sh
+++ test-network/startNetwork.sh
@@ -4,6 +4,8 @@
 
 ./network.sh up createChannel -s couchdb -c mychannel -ca
 
-./network.sh deployCC -ccn ledger -ccp ../chaincode-go/ -ccl go -ccep "OR('Org1MSP.peer','Org2MSP.peer')"
+# ./network.sh deployCC -ccn ledger -ccp ../chaincode-go/ -ccl go -ccep "OR('Org1MSP.peer','Org2MSP.peer')"
 
 # ./network.sh deployCC -ccn ledger -ccp ../chaincode-go/ -ccl go -ccep "AND('Org1MSP.peer','Org2MSP.peer')"
+
+./network.sh deployCC -ccn basic -ccp ../token-erc-20/chaincode-go/ -ccl go -ccep "AND('Org1MSP.peer','Org2MSP.peer')"
